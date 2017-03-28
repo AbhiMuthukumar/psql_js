0da30a94814fbe5b9ea7b06b9b3ea7cd7f600371
Abhi Muthukumar <abhi.muthukumar@gmail.com>
2017-03-28 13:42:23 -0400

    done with assignment 1 for the day

---
+++ test-script.js
@@ -0,0 +1,36 @@
+const pg = require("pg");
+const settings = require("./settings.json");
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.host,
+  port: settings.port,
+  sl: settings.ssl
+});
+let query = "";
+let result = [];
+
+function getResults(name,callback){
+  client.connect((err) => {
+    if(err){
+      return console.error("Conection Error: ", err);
+    }
+    client.query("SELECT * from famous_people where name = $1 or lastname = $1",[name], (err,result) => {
+      console.log("searching...");
+      if(err){
+        console.log("Error running the Query :" ,err);
+      }
+      callback(result)
+    });
+  });
+}
+
+getResults(process.argv[2], (result) => {
+  console.log(`Found ${result.rows.length} persons(s) by the name '${process.argv[2]}'`);
+  for(let i=0; i<result.rows.length; i++){
+    console.log(`- ${i+1} : ${result.rows[i].name} ${result.rows[i].lastname}, born '${result.rows[i].dob.getFullYear()}-${("0"+(result.rows[i].dob.getMonth()+1)).slice(-2)}-${result.rows[i].dob.getDate()}'`);
+  }
+  client.end();
+});
